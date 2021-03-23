bff252d2ca44052ee81a93421f8c5181d7809e0d
Nicolas <nbernalc@misena.edu.co>
2021-03-23 12:59:22 -0500

    confirmar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          |<code>Nicolas Bernal Cruz</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
