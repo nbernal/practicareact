d242de647d7d3ae01c113356e559a1de4e9cb3cc
Nicolas <nbernalc@misena.edu.co>
2021-03-23 16:42:03 -0500

    confirmar

--- src/App.js
+++ src/App.js
@@ -30,13 +30,9 @@ function App() {
         <div className={classes.root}>          
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
+          <Paper className={classes.paper}>
+
+          </Paper>
         </Grid>
         <Grid item xs={3}>
           <Paper className={classes.paper}>xs=3</Paper>
