788d7dd425eef7ce9c55d014968714af55cf46bd
Nicolas <nbernalc@misena.edu.co>
2021-03-23 15:42:14 -0500

    confirmar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
 
-import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import React,{useState, useEffect} from 'react';
+import './App.css';
 
 function App() {
+  const [currentSum,setCurrentSum]=useState(0);
+  const [clear,setClear]=useState(false);
+
+  useEffect(()=>{
+    document.querySelector('#result').value="";
+  },[])
+  
+  useEffect(()=>{
+    if(clear)
+    document.querySelector('#result').value="";
+  })
+
+  const Add=(e)=>{
+    e.preventDefault();
+    if(clear) setClear(false);
+    let currentNum=document.querySelector('#num').value
+    if(currentNum=='')
+    return;
+    let sum= currentSum+parseInt(currentNum);
+    setCurrentSum(sum);
+    document.querySelector('#num').value="";
+      
+  }
+
+  const Clear=(e)=>{
+    e.preventDefault();
+    console.log('sum:', currentSum);
+    document.querySelector('form').reset();
+    setClear(true);
+    setCurrentSum(0);
+  }
+
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="sm">
-        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
-      </Container>
-    </React.Fragment>
+    <div className="App">
+      <div className="app-title">
+        <h1> Basic Form Calculator</h1>
+      </div>
+      <form>
+            <input type="text" id="result" value={currentSum}  readOnly />   
+            <input type="text" id="num" placeholder="enter a number" />
+            <button onClick={Add}>Add</button>
+            <button onClick={Clear}>Clear</button>
+      </form>
+    </div>
   );
 }
 
